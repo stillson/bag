77e54dd9173ae795d2a0048672161ff617bc3fed
Chris Stillson <stillson@gmail.com>
2017-10-12 14:47:31 -0700

    added missing comma

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension, find_packages
 
 setup(name='bag',
       version='0.1',
-      description="python bag class"
+      description="python bag class",
       author="Chris Stillson",
       author_email="stillson@gmail.com",
       license="New BSD license",
